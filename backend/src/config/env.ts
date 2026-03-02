205fb47cdef29451b117b18e996688b8652f12f5
Francis Randrianarivony <f6randrianarivony@gmail.com>
2026-03-02 08:50:38 +0300

    initial commit

---
+++ backend/src/config/env.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true }); // Load environment variables from .env file
+
+export const ENV = {
+  PORT: process.env.PORT || 3000, // Default to 3000 if PORT is not set
+  DB_URL: process.env.DB_URL, // Database URL for connecting to the database
+  NODE_ENV: process.env.NODE_ENV, // Environment (development, production, etc.)
+  FRONTEND_URL: process.env.FRONTENS_URL, // Frontend URL for CORS configuration
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY, // Clerk publishable key for authentication
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY, // Clerk secret key for authentication
+};
---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { ENV } from "./config/env";
+import cors from "cors";
+
+import { clerkMiddleware } from "@clerk/express";
+
+const app = express();
+
+app.use(clerkMiddleware()); // Use Clerk middleware for authentication
+// auth obj will be attached to req obj
+app.use(cors({ origin: ENV.FRONTEND_URL })); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // parses form data (like from HTML forms)
+
+app.get("/", (req, res) => {
+  res.json({
+    message:
+      "welcome to the Futurify API - Powered by codeline401 on : Postgresql, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "api/users",
+      products: "api/products",
+      comments: "api/comments",
+    },
+  });
+});
+
+app.listen(ENV.PORT, () =>
+  console.log(`Server is running on port ${ENV.PORT}`),
+);
